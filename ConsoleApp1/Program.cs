f10076827cfaab5adda15f787117350436f73188
mattia battecca <mattia.battecca.studenti@isii.it>
2020-11-14 15:25:37 +0100

    programma casuale Commit 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ELIMINA RIPETIZIONI
+
+            List<string> ls = new List<string>();
+            List<string> ls1 = new List<string> { "cane", "cane", "cane", "casa", "albero", "cane", "casa", "casa" };
+
+            foreach (var s in ls1)
+            {
+                if (!ls.Contains(s))
+                    ls.Add(s);
+            }
+
+            foreach (var s in ls)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
